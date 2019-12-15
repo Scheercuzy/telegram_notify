31acb107512d4681ffdc9ab4bfe5c053fd2a8738
Maxence Scheercousse <maxence.scheercousse@flymya.co>
2019-12-15 19:28:27 +0630

    Checks if request is json + msg key exists

--- telegram_notify/webserver.py
+++ telegram_notify/webserver.py
@@ -7,7 +7,14 @@ app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def index():
+    if not request.is_json:
+        return "Error, only accepts json"
+
     data = request.get_json()
+
+    if "msg" not in data:
+        return "Missing msg key"
+
     bot.send_message(
         chat_id=CHAT_ID,
         text=data["msg"])
